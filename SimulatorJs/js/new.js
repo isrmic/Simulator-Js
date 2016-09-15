99205f16b34bb65ba686aa7879135b4c138a2c01
isrmic <israelfcfm@outlook.com>
2016-09-15 15:20:36 -0300

    newjs modified

--- SimulatorJs/js/new.js
+++ SimulatorJs/js/new.js
@@ -290,19 +290,3 @@ var func;
 
 
 })();
-
-var query = location.search.slice(1);
-var partes = query.split('&');
-var data = {};
-partes.forEach(function (parte) {
-    var chaveValor = parte.split('=');
-    var chave = chaveValor[0];
-    var valor = chaveValor[1];
-    data[chave] = valor;
-});
-
-
-
-function callback(){
-  alert('acionou o callback');
-}
